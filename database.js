6633dd019582ed830fbb1e380301d373add5b95a
naphat245 <118192913+naphat245@users.noreply.github.com>
2024-04-18 01:34:57 +0700

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-const dbConnection = mysql.createPool({
-    host     : 'localhost', // MYSQL HOST NAME
-    user     : 'root', // MYSQL USERNAME
-    password : '', // MYSQL PASSWORD
-    database : 'seat_bar_db' // MYSQL DB NAME
-}).promise();
-module.exports = dbConnection;
