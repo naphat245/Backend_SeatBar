aafbdddbec8616f8044663d849c4a1010007a3bb
naphat245 <118192913+naphat245@users.noreply.github.com>
2024-04-18 01:34:35 +0700

    Delete routes directory

--- routes/AuthRoutes.js
+++
@@ -1,9 +0,0 @@
-const authRoute = require('express').Router()
-const authController =require('../controllers/AuthControllers')
-
-authRoute.post('/register',authController.register)
-authRoute.get('/users',authController.users)
-authRoute.post('/login',authController.login)
-
-
-module.exports = authRoute
